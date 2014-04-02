11052b08cbe7d51d7091d7fd4533a9620cf3bfe8
lpaulger <lucas.paulger@gmail.com>
2014-04-02 17:45:26 +0300

    travis-ci

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,14 +3,14 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('lp-cordova generator', function () {
+describe('ng-cordova generator', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
         return done(err);
       }
 
-      this.app = helpers.createGenerator('lp-cordova:app', [
+      this.app = helpers.createGenerator('ng-cordova:app', [
         '../../app'
       ]);
       done();
--- test/test-load.js
+++ test/test-load.js
@@ -2,7 +2,7 @@
 'use strict';
 var assert = require('assert');
 
-describe('lp-cordova generator', function () {
+describe('ng-cordova generator', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
     assert(app !== undefined);
